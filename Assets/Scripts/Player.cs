b981699d273a0d394ed6e3eef0c985650b7fc2f6
RyanKoech <sirryankoech@gmail.com>
2023-11-20 21:12:22 +0300

    add player speed increase

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,10 +7,13 @@ public class Player : MonoBehaviour
 
     public float speed;
     public float jump;
+    public float mileStone;
+    public float speedMultiplier;
     public LayerMask ground;
     public AudioSource deathSound;
     public AudioSource jumpSound;
 
+    private float mileStoneCount;
     private Rigidbody2D rigidBody;
     private Collider2D playerCollider;
     private Animator animator;
@@ -19,9 +22,17 @@ public class Player : MonoBehaviour
         rigidBody = GetComponent<Rigidbody2D>();
         playerCollider = GetComponent<Collider2D>();
         animator = GetComponent<Animator>();
+        mileStoneCount = mileStone;
     }
     void Update()
     {
+
+        if (transform.position.x > mileStoneCount) {
+            mileStoneCount += mileStone;
+            speed = speed * speedMultiplier;
+            mileStone += mileStone * speedMultiplier;
+        }
+
         rigidBody.velocity = new Vector2(speed, rigidBody.velocity.y);
 
         bool grounded = Physics2D.IsTouchingLayers(playerCollider, ground);
