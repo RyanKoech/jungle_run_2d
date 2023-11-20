a0fc1a102dffe47a4ac233265428c693035e7925
RyanKoech <sirryankoech@gmail.com>
2023-11-20 18:03:11 +0300

    add coin picking

---
+++ Assets/Scripts/CoinPicker.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinPicker : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D other) {
+        if (other.gameObject.name == "Player") {
+            gameObject.SetActive(false);
+        }
+    }
+}
